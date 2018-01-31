9cb328c50b8090c81180c3e4e1fd244a1bc35062
Prajna12 <30750187+Prajna12@users.noreply.github.com>
2018-01-31 07:08:44 +0530

    implemented integration

--- src/SignUp.js
+++ src/SignUp.js
@@ -5,6 +5,13 @@ export default class SignUp extends React.Component {
   static navigationOptions = {
     title: "Sign Up"
   }
+  state = {
+    loading: true,
+    error: false,
+    posts: [],
+  }
+  
+
   render() {
     const { navigate } = this.props.navigation
     return (
@@ -13,16 +20,19 @@ export default class SignUp extends React.Component {
           style={{height: 40}}
           placeholder="Username"
           onChangeText={(text) => this.setState({text})}
+          uname={this.state.text}
         />
          <TextInput
          style={{height: 40}}
           placeholder="Email Id"
           onChangeText={(text) => this.setState({text})}
+          mail={this.state.text}
         />
           <TextInput
          style={{height: 40}}
           placeholder="Set Password"
           onChangeText={(text) => this.setState({text})}
+          pass={this.state.text}
         />
     <Button
   onPress={() => navigate("Login", {screen: "Login"})}
@@ -33,6 +43,38 @@ export default class SignUp extends React.Component {
 <View style={{ height: 60 }} />
 </KeyboardAvoidingView>
     );
+  }
+  componentWillMount = async () => {
+    let username = "uname"
+    let password = "pass"
+    let signupUrl = 'https://app.alkalize14.hasura-app.io/signup'
+    let requestOptions = {
+      "method": "POST",
+      "headers": {
+        "Content-Type":"application/json"
+      }
+    };
+  
+    let body = {
+      "provider":"username",
+      "data": {
+        "username": username,
+        "password": password
+      }
+    };
+  
+    requestOptions["body"] = JSON.stringify(body);
+    console.log("Auth Response ---------------------");
+    
+    try {
+      let resp = await fetch(signupUrl, requestOptions);
+      console.log(resp);
+      return resp; 
+    }
+    catch(e) {
+      console.log("Request Failed: " + e);
+      return networkErrorObj;
+    }
   }
 }
 
